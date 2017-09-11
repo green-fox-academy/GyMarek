70cad3c995ac2b40a69bd0dba686fa03a0260a1e
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 15:00:24 +0200

    ex13

---
+++ week-02/Day-01/ex13_Seconds/ex13_Seconds/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex13_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            Console.WriteLine(24*60*60-currentHours*60*60-currentMinutes*60-currentSeconds);
+
+            Console.ReadLine();
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+        }
+    }
+}
