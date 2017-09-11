34bbf58e6b71f81ae076dd0c2e99c5a7605b2e30
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 12:36:18 +0200

    ex04

---
+++ week-02/Day-01/ex04_Intro/ex04_Intro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex04_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marek");
+            Console.WriteLine("33");
+            Console.WriteLine("1.77");
+
+            Console.ReadLine();
+        }
+    }
+}
