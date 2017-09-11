371fef7960ff172f91638071692969acc22ba94d
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 15:16:47 +0200

    ex15

---
+++ week-02/Day-01/ex15_MileToKm/ex15_MileToKm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex15_MileToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ChangeNumber = 0.621371;
+                      
+            Console.WriteLine("How many kms?");
+
+            string km = Console.ReadLine();
+            double mile = Convert.ToDouble(km);
+
+            Console.WriteLine(mile*ChangeNumber + " miles");
+           
+            Console.ReadLine();
+
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+        }
+    }
+}
