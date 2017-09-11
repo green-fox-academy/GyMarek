26547dbbd5cd27fb802d7c987dc063ebc1d2c9e3
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 13:45:11 +0200

    ex10

---
+++ week-02/Day-01/Ex10_Define/Ex10_Define/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex10_Define
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Marek";
+            int age = 33;
+            double height = 1.77;
+            bool married = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+
+            Console.ReadLine();
+
+        }
+    }
+}
