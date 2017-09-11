45eb9d795cc4da55e71654154fd7be457703ede0
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 22:57:13 +0200

    ex23

---
+++ week-02/Day-01/ex23_IWontCheat/ex23_IWontCheat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ex23_IWontCheat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
