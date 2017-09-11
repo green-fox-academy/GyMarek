cd354fb74fca4ce216ed0d69bced547554b59328
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 15:33:22 +0200

    ex16

---
+++ week-02/Day-01/ex16_AnimalsAndLegs/ex16_AnimalsAndLegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex16_AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many chickens?");
+
+            string chikens = Console.ReadLine();
+            double Clegs = Convert.ToDouble(chikens);
+
+            Console.WriteLine("How many pigs");
+
+            string pigs = Console.ReadLine();
+            double Plegs = Convert.ToDouble(pigs);
+
+            double sum = 2 * Clegs + 4 * Plegs;
+
+            Console.WriteLine("The number of legs: " + sum);
+
+            Console.ReadLine();
+
+
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+        }
+    }
+}
