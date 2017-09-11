a446873baa66edec78f7fd1f61dfec032e3d1ef0
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 13:22:31 +0200

    ex07

---
+++ week-02/Day-01/ex07_Favourite/ex07_Favourite/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex07_Favourite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fav = 7;
+            Console.WriteLine("My favourite number is: " + fav);
+
+            Console.ReadLine();
+        }
+    }
+}
