72252589d3e2c278069ca445ee4a49179e837c6c
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 12:43:08 +0200

    Ex05

---
+++ week-02/Day-01/Ex05_TwoNumbers/Ex05_TwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex05_TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 13;
+            int b = 22;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(b - a);
+            Console.WriteLine(a * b);
+            Console.WriteLine(22.0 / 13.0);
+            Console.WriteLine(b / a);
+            Console.WriteLine(b % a);
+
+            Console.ReadLine();
+        }
+    }
+}
