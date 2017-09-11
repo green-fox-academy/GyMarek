cca27dcc3223142698ea79a980ce7471aef88582
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 13:15:37 +0200

    ex06

---
+++ week-02/Day-01/ex06_Coding/ex06_Coding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex06_Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daily = 6;
+            int weeks = 17;
+            int weeksday = 5;
+            double weeklyhour = 52;
+
+            Console.WriteLine(daily*weeks*weeksday);
+            Console.WriteLine((daily*weeks*weeksday)/(weeks*weeklyhour)*100 + "%");
+
+            Console.ReadLine();
+        }
+    }
+}
