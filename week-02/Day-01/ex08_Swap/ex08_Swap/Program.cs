3664ee3c32c0eba36da5cc5f7a2d9efd615c83f5
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 13:35:28 +0200

    ex08

---
+++ week-02/Day-01/ex08_Swap/ex08_Swap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex08_Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
