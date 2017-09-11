d7793a4beb61e606b1f3b38f179913a0a5cf439c
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 15:04:41 +0200

    ex14

---
+++ week-02/Day-01/ex14_helloUser/ex14_helloUser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex14_helloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name + "!");
+           
+            Console.ReadLine();
+
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+        }
+    }
+}
