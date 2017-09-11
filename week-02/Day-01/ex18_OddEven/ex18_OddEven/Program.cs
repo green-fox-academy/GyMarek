170277c0c2c4c3c52700b28fdb83b97d30f4c291
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 16:29:37 +0200

    ex18

---
+++ week-02/Day-01/ex18_OddEven/ex18_OddEven/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex18_OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls write an integer:");
+            
+            string message = Console.ReadLine();
+            int number = Int32.Parse(message);
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is an even");
+            }
+            else 
+            {
+                Console.WriteLine("The number is an odd");
+            }
+                        
+            Console.ReadLine();
+
+
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+        }
+    }
+}
