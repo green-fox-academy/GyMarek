901e7d496fa965748ba5085fac6be1839f9bd224
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 12:29:28 +0200

    ex03

---
+++ week-02/Day-01/ex03_HelloOthers/ex03_HelloOthers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex03_HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Boldi!");
+            Console.WriteLine("Hello, Arnold!");
+            Console.WriteLine("Hello, Lóci!");
+
+            Console.ReadLine();
+        }
+    }
+}
