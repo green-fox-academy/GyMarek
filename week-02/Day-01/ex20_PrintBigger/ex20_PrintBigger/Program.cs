02a8b4939c84e75bcb9553f5af29c6c00dee56af
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 16:52:46 +0200

    ex20

---
+++ week-02/Day-01/ex20_PrintBigger/ex20_PrintBigger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ex20_PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls write an integer");
+
+            string one =Console.ReadLine();
+            int a = Int32.Parse(one);
+
+            Console.WriteLine("Pls write another integer");
+
+            string two = Console.ReadLine();
+            int b = Int32.Parse(two);
+
+            if (a>b)
+            {
+                Console.WriteLine("The first number is the bigger one!");
+            }
+             else if (b>a)
+            {
+                Console.WriteLine("The second number is the bigger one!");
+            }
+            else
+            {
+                Console.WriteLine("Equals! :)");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
