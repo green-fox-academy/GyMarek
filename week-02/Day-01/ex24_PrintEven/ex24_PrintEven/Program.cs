45ea1a7d63ccb177429f5d1226e216b4200e3afc
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 23:05:55 +0200

    ex24

---
+++ week-02/Day-01/ex24_PrintEven/ex24_PrintEven/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex24_PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 500; i +=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
