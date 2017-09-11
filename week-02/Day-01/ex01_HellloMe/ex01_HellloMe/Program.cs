7594ceee5e5294af53001c819b31760026697294
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 12:03:13 +0200

    ex01

---
+++ week-02/Day-01/ex01_HellloMe/ex01_HellloMe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex01_HellloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Marek");
+            Console.ReadLine();
+        }
+    }
+}
