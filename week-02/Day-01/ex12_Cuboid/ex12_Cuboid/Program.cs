72d986f8b25e76d0670083296354f04a5aa08e56
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-11 14:39:51 +0200

    ex12

---
+++ week-02/Day-01/ex12_Cuboid/ex12_Cuboid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex12_Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "pls add the side long of the cuboid";
+            string side;
+            double sideValue;
+
+            Console.WriteLine(message);
+
+            side = Console.ReadLine();
+            sideValue = Convert.ToDouble(side);
+
+            Console.WriteLine("Surface Area: " + sideValue*sideValue*6);
+            Console.WriteLine("Volume: " + sideValue*sideValue*sideValue);
+
+            Console.ReadLine();
+        }
+    }
+}
