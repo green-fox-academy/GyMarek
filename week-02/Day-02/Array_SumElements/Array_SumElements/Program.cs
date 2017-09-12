d1f477cea1b2b4d798992b4fb89a618656bcde6e
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 22:54:24 +0200

    Array_SumElements

---
+++ week-02/Day-02/Array_SumElements/Array_SumElements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array_SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = {54, 23, 66, 12};
+
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            // - Print the sum of the second and the third element
+
+            Console.WriteLine(r[1] + r[2]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
