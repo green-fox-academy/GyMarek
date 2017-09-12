7fd7b2e80cb36b1a8f39a6aead2f3eb7e3846663
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 23:36:04 +0200

    Array_Matrix

---
+++ week-02/Day-02/Array_Matrix/Array_Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Array_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+                
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+                // - Create (dynamically) a two dimensional array
+                //   with the following matrix. Use a loop!
+                //
+                //   1 0 0 0
+                //   0 1 0 0
+                //   0 0 1 0
+                //   0 0 0 1
+                //
+                // - Print this two dimensional array to the output
+
+                Console.ReadLine();
+        }
+    }
+}
