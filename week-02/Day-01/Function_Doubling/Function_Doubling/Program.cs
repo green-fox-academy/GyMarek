4e6c69c6b251eabf66f2ec90b67435a20f8f61b6
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 13:28:01 +0200

    Function_Doubling

---
+++ week-02/Day-01/Function_Doubling/Function_Doubling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+        }
+
+        public static int Doubling (int num)            
+        {
+            return num *= 2;
+        }
+    }
+}
