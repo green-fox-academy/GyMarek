cb1760e86acfde5e7b356f335a4a817c3633c792
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 22:28:35 +0200

    Array_Third

---
+++ week-02/Day-02/Array_Third/Array_Third/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Array_Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = { 4, 5, 6, 7 };
+
+            // - Create an array variable named `q`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+           
+            Console.WriteLine(q[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
