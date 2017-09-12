f5471fa53452c967c884649f8cc520600176b964
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 17:12:05 +0200

    Summing

---
+++ week-02/Day-02/Function_Summing/Function_Summing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls write an integer");
+
+            string one = Console.ReadLine();
+            int a = Int32.Parse(one);
+
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            Console.WriteLine(Sum(a));
+            Console.ReadLine();
+        }
+
+        public static int Sum (int num)
+        {
+            int b = num;
+
+            for (int i=0; i<b; i++)
+            {
+                num += i;
+            }
+            return num;
+        }
+    }
+}
