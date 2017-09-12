2955bf9243f326dbd8727335f07967e21644adf7
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 23:47:49 +0200

    Array_DoubleItems

---
+++ week-02/Day-02/Array_DoubleItems/Array_DoubleItems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array_DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+
+            for (int i = 0; i < ag.Length; i++)
+            {
+                Console.WriteLine(ag[i] *= 2);
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
