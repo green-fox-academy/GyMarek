bd3b36027b43c17ceadd6964f6bcf984ec9fa994
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 23:04:43 +0200

    Array_IncrementElements

---
+++ week-02/Day-02/Array_IncrementElement/Array_IncrementElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = { 1, 2, 3, 4, 5 };
+
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+
+            t[2] ++;
+
+            Console.WriteLine(t[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
