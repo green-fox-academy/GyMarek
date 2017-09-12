14fb3a789cd7c98b1e95c0c3f930dd0edcb876b6
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 22:37:33 +0200

    Array_CompareLength

---
+++ week-02/Day-02/Array_CompareLength/Array_CompareLength/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Array_CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3};
+            int[] p2 = { 3, 5};
+
+            // - Create an array variable named `p1`
+            //   with the following content: `[1, 2, 3]`
+            // - Create an array variable named `p2`
+            //   with the following content: `[4, 5]`
+            // - Print if `p2` has more elements than `p1`
+
+            if (p2.Length > p1.Length)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
