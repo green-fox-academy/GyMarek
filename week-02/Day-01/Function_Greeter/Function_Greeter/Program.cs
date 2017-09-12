90e292f07a0adb640df29c4a92f9263688c566c7
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 13:43:32 +0200

    Function_Greeter

---
+++ week-02/Day-01/Function_Greeter/Function_Greeter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Function_Greeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            Console.WriteLine(Greet(al));
+            Console.ReadLine();
+        }
+
+        public static string Greet (string name)
+        {
+            return name = "Greetings dear, " + name;
+        }
+    }
+}
+
+
