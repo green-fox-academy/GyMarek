fe273f637f2ac3714c550cfe8a178b20c0b35012
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 23:09:20 +0200

    Array_PrintAll

---
+++ week-02/Day-02/Array_PrintAll/Array_PrintAll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = {4, 5, 6, 7};
+
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+
+            foreach(int i in af)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
