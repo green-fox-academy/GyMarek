1341acaa6b7db838b99a25dd7ef991f418a1dd33
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-12 17:17:40 +0200

    Factorial

---
+++ week-02/Day-02/Function_Factorio/Function_Factorio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls write an integer");
+
+            string one = Console.ReadLine();
+            int a = Int32.Parse(one);
+
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+            Console.WriteLine(Factorio(a));
+            Console.ReadLine();
+        }
+
+        public static int Factorio (int num)
+        {
+            int b = num;
+
+            for (int i = 1; i < b; i++)
+            {
+                num *= i;
+            }
+            return num;
+        }
+    }
+}
