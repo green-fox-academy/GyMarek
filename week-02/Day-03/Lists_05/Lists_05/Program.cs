6a0a620b4bfb8e6ff9334fab57898df1a26543b2
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-13 17:27:24 +0200

    Lists_05

---
+++ week-02/Day-03/Lists_05/Lists_05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+
+            // First Solution
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hoorray");
+            }
+            else
+            {
+                Console.WriteLine("Noooooo");
+            }
+
+            // Second Solution
+
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i] == 7)
+                {
+                    Console.WriteLine("Hoorray");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Noooooo");
+
+
+            Console.ReadLine();
+        }
+    }
+}
