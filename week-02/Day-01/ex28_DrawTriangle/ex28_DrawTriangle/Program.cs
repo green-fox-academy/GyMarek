be7f8962f7d0f7bce8100c3fce1768c682997e15
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-14 16:26:34 +0200

    ex28_DrawTriangle

---
+++ week-02/Day-01/ex28_DrawTriangle/ex28_DrawTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex28_DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            Console.WriteLine("Please give the base of the triangle:");
+            int triangle = Int32.Parse(Console.ReadLine());
+            string star = "*";
+
+            for (int i = 0; i < triangle ; i++)
+            {
+                Console.WriteLine(star);
+                star += "*";
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
