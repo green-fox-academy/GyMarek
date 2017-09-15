2b7e4083fe547e9a06d07724d7c4df3fff6b5f9f
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-15 11:20:46 +0200

    Anagram

---
+++ week-02/Day-05/Anagram/Anagram/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a word:");
+            string first = Console.ReadLine();
+            first = first.Replace(" ", "");
+            char[] firstChars = first.ToCharArray();
+            Array.Sort(firstChars);
+
+            Console.WriteLine("Please write another word t" +
+                "o check it is an anagram or no:");
+            string second = Console.ReadLine();
+            second = second.Replace(" ", "");
+            char[] secondChars = second.ToCharArray();
+            Array.Sort(secondChars);
+
+            if (firstChars.Length != secondChars.Length)
+            {
+                Console.WriteLine("Not anagrams :(");
+            }
+            else
+            {
+                for (int i = 0; i < firstChars.Length; i++)
+                {
+                    if (firstChars[i] != secondChars[i])
+                    {
+                        Console.WriteLine("Not anagrams :(");
+                    }                    
+                }
+            }
+
+            Console.WriteLine("The second one is an anagram :)");
+            
+            Console.ReadLine();
+        }
+    }
+}
