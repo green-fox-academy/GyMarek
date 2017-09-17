798928bae3515974be544e6177a1714461bb2d4e
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-17 14:14:36 +0200

    ex34_ParametricAverage

---
+++ week-02/Day-01/ex34_ParametricAverage/ex34_ParametricAverage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex34_ParametricAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me how many numbers do yo want to test:");
+            int UserNumber = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < UserNumber; i++)
+            {
+                Console.WriteLine("Please give me another number:");
+                int number = Int32.Parse(Console.ReadLine());
+                sum += number;            
+            }
+
+            int average = sum / UserNumber;
+
+            Console.WriteLine();
+            Console.WriteLine("Sum: " + sum + " Average: " + average);
+            Console.ReadKey();
+
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            Console.ReadLine();
+        }
+    }
+}
