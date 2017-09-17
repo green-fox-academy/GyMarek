8bdade30dfd80a7af768c957f6d829b0a8e04ea1
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-17 15:44:15 +0200

    Array_AppendA

---
+++ week-02/Day-02/Array_AppendA/Array_AppendA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = new string[] { "kuty", "macsk", "kacs", "rók", "halacsk" };           
+
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i] += "a";                
+            }
+
+            foreach (string anima in animals)
+            {
+                Console.WriteLine(anima);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
