845dbb1d2ffe19892e0063a72f41428a44c34de4
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-17 13:37:27 +0200

    ex032_Diagonal

---
+++ week-02/Day-01/ex31_Square/ex31_Square/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex31_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give the square lines:");
+            int line = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i < line; i++)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine("%%%%%");
+                }
+                if (i == line - 1)
+                {
+                    Console.WriteLine("%%%%%");
+                }
+                else
+                {
+                    Console.WriteLine("%   %");
+                }
+            }
+
+            Console.ReadKey();
+
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+        }
+    }
+}
---
+++ week-02/Day-01/ex32_Diagonal/ex32_Diagonal/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex32_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give the diagonal lines:");
+            int diagonal = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < diagonal; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < diagonal - 2; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.Write("%");
+                }                    
+                for (int j = 0; j < diagonal - 2; j++)
+                {
+                    if (j == i)
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                for (int j = 0; j < 1; j++)
+                    Console.Write("%");
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < diagonal; i++)
+            {
+                Console.Write("%");
+            }            
+
+            Console.ReadKey();
+
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+        }
+    }
+}
