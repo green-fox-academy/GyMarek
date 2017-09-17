02cf8c1790ff579badcc6ce1675a90b88f60e130
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-17 14:43:38 +0200

    ex35_ChessTable

---
+++ week-02/Day-01/ex35_DrawChessTable/ex35_DrawChessTable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex35_DrawChessTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here is your chess table :)");
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("% % % % ");
+                }
+                else
+                {
+                    Console.WriteLine(" % % % %");
+                }
+            }
+
+            Console.ReadLine();
+
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+        }
+    }
+}
