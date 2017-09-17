ce63cc993ddda5b9260199e6a2eecfc09a530f75
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-17 16:57:07 +0200

    Array_SumAll

---
+++ week-02/Day-02/Array_SumAll/Array_SumAll/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = new int[] { 3, 4, 5, 6, 7 };
+
+            int sum = 0;
+            for (int i = 0; i < ai.Length; i++)
+            {
+                sum += ai[i];
+            }
+
+            Console.WriteLine("Sum of the array: " +sum);
+
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+
+
+            Console.ReadLine();
+        }
+    }
+}
