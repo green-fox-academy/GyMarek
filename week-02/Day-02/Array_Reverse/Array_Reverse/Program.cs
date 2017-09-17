53da3e5c022c41c53d0e2b4e18f6c34c8d9a84df
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-17 17:09:50 +0200

    Array_Reverse

---
+++ week-02/Day-02/Array_Reverse/Array_Reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = new int [] {3, 4, 5, 6, 7};
+
+            foreach (int i in aj)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine();
+
+            Array.Reverse(aj);
+
+            foreach (int i in aj)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            Console.ReadLine();
+        }
+    }
+}
