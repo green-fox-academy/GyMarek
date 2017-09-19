e9d9da158f1aa4639edaf780c1f803a7a2a16f1a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-19 21:36:47 +0200

    Ex04_WriteSingleLine

---
+++ week-03/Day-02/Ex04_WriteSingleLine/Ex04_WriteSingleLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04_WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"./my-file.txt";
+          
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("Gyula Nemes");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: my - file.txt");
+            } 
+            
+            Console.ReadLine();
+            // Open a file called "my-file.txt"
+            // Write your name in it as a single line
+            // If the program is unable to write the file,
+            // then it should print an error message like: "Unable to write file: my-file.txt"
+        }        
+    }
+}
