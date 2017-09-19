ea83b2b3f5a8b6b1542a55b997a62e5717051875
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-19 13:08:36 +0200

    Ex01-DivideByZero

---
+++ week-03/Day-02/Ex01_DivideByZero/Ex01_DivideByZero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+
+            Console.WriteLine("Pls give a number");
+            int InputNumber = Int32.Parse(Console.ReadLine());
+            Divide(InputNumber);
+            Console.ReadLine();
+        }
+
+        public static int Divide (int num)
+        {      
+            try  
+            {
+                int result = 10 / num;               
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {            
+                Console.WriteLine("Fail");
+            }            
+            return num;
+        }
+    }
+}
