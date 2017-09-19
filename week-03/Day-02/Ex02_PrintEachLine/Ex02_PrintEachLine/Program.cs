627a46bbe35c0208adc0f1be220429ad59d950c7
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-19 13:28:36 +0200

    Ex02_PrintEachLine

---
+++ week-03/Day-02/Ex02_PrintEachLine/Ex02_PrintEachLine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Ex02_PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"./my-file.txt";
+            try
+            {
+                string[] content;
+                content = File.ReadAllLines(path);                
+
+                foreach (string line in content)
+                {
+                   Console.WriteLine(line); 
+                }                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+
+            Console.ReadLine();
+
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each of lines form the file.
+            // If the program is unable to read the file (for example it does not exists),
+            // then it should print an error message like: "Unable to read file: my-file.txt"
+        }
+    }
+}
