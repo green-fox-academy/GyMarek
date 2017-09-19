c6746ba1a79319f18c4959c48534749c1ea49332
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-19 23:30:20 +0200

    Ex07_Doubled_NotFinal

---
+++ week-03/Day-02/Ex07_Doubled/Ex07_Doubled/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex07_Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathfrom = @"./duplicated-chars.txt";
+            string pathto = @"./chars.txt.txt";
+
+            Decrypts(pathfrom,pathto);          
+
+            Console.ReadLine();
+
+            // Create a method that decrypts the duplicated-chars.txt 
+        }
+
+
+        static void Decrypts(string pathfrom, string pathto)
+        {
+            string[] content = File.ReadAllLines(pathfrom);
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                var text = new List<char>(content[i]);
+
+                for (int j = 0; j < text.Count - 1; j++) //not final, find a solution to ignore this step
+                {
+                    text.RemoveAt(j);
+                }
+                content[i] = string.Join("", text.ToArray());
+            }
+            File.WriteAllLines(pathto, content);
+            Console.WriteLine("Copy was successful");
+        }
+    }
+}
