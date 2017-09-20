802542d9585a82ad90510c2210135fb5eb86ed0f
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-20 13:43:59 +0200

    Ex07_FourRectangle

---
+++ week-03/Day-03/Ex07_FourRectangles/Ex07_FourRectangles/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ex07_FourRectangles
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw four different size and color rectangles.
+            Random r = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                foxDraw.FillColor(Color.FromRgb((byte)r.Next(255),
+                (byte)r.Next(255), (byte)r.Next(255)));
+                foxDraw.DrawRectangle(r.Next(300), r.Next(300), r.Next(100), r.Next(100));
+            }            
+        }
+    }
+}
