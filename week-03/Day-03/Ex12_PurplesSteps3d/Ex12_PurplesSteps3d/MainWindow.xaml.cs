801489e073013e0870d5fc03c3a440cdcf0bbbc9
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-20 16:39:57 +0200

    Ex12_PurpleSteps3d

---
+++ week-03/Day-03/Ex12_PurplesSteps3d/Ex12_PurplesSteps3d/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ex12_PurplesSteps3d
+{   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // reproduce this:
+            // r4.png
+
+            int position = 10;
+            int increase = 20;
+            int noOfBoxes = 6;
+            
+            for (int size = 0; size <= noOfBoxes* increase; size+= increase)
+            {
+                foxDraw.FillColor(Colors.Purple);
+                foxDraw.DrawRectangle(position, position, size, size);
+                position += size;
+            }
+        }
+    }
+}
