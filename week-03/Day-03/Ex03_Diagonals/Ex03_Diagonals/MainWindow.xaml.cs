362619a1b4665b0f5cb8ff5adf8c744a16c74a51
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-20 11:42:23 +0200

    Ex03_Diagonals

---
+++ week-03/Day-03/Ex03_Diagonals/Ex03_Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ex03_Diagonals
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw the canvas' diagonals in green.
+
+            var pointList = new List<Point>()
+            {
+                new Point(canvas.Width/3, canvas.Height/3),
+                new Point(canvas.Width/3*2, canvas.Height/3),
+                new Point(canvas.Width/3*2, canvas.Height/3*2),
+                new Point(canvas.Width/3, canvas.Height/3*2),
+            };
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(pointList[0], pointList[2]);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(pointList[1], pointList[3]);            
+        }
+    }
+}
