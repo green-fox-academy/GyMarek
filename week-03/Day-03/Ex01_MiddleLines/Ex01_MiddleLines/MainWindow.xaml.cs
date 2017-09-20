036e6b4738c11125f5444b198308f376666857db
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-20 11:09:46 +0200

    Ex01_MiddleLines

---
+++ week-03/Day-03/Ex01_MiddleLines/Ex01_MiddleLines/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Ex01_MiddleLines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            //  draw a red horizontal line to the canvas' middle.
+            //  draw a green vertical line to the canvas' middle.
+
+            var startPointHor = new Point(0, canvas.Height/2);
+            var endPointHor = new Point(canvas.Width, canvas.Height/2);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(startPointHor,endPointHor);
+
+            var startPointVer = new Point(canvas.Width / 2, 0);
+            var endPointVer = new Point(canvas.Width/2, canvas.Height);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(startPointVer,endPointVer);            
+        }
+    }
+}
