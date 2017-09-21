e0babb56a4c43a9e96e103636b69ca1524c0d807
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-21 16:13:19 +0200

    Ex09_StringThree

---
+++ week-03/Day-04/Ex09_StringThree/Ex09_StringThree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09_StringThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+
+            string input = "Xoxoxoxoxoxo";
+            Console.WriteLine(Separate(input));
+            Console.ReadLine();
+        }
+        public static string Separate(string input)
+        {
+            if (input.Length < 1)
+                return input;
+            else
+                return input[0] + " " + Separate(input.Substring(1));
+        }
+    }
+}
