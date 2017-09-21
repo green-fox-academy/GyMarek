5ea390ba17074180793dea47e710c1d2956538ae
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-21 10:28:02 +0200

    Ex01_Counter

---
+++ week-03/Day-04/Ex01_Counter/Ex01_Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+
+            countDown(15);
+            Console.ReadLine();
+        }
+        public static void countDown(int n)
+        {
+            if (n < 0)
+                return ;
+            Console.WriteLine(n);
+            countDown(n - 1);
+        }
+    }
+}
