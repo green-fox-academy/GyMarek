5eb5b355a831abc0bb85daa0ea11be2cf25710b0
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-25 13:44:25 +0200

    Ex02_Blogspot

---
+++ week-04/Day-01/Ex02_BlogSpot/Ex02_BlogSpot/BlogPost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_BlogSpot
+{
+    class BlogPost
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
+
+        // class --> villanykorte baloldalon --> Generate Constructor... :
+
+        //public BlogPost(string authorName, string title, string text, string publicationDate)
+        //{
+        //    AuthorName = authorName;
+        //    Title = title;
+        //    Text = text;
+        //    PublicationDate = publicationDate;
+        //}
+
+        public BlogPost(string AuthorName, string Title, string Text, string PublicationDate)
+        {
+            this.AuthorName = AuthorName;
+            this.Title = Title;
+            this.Text = Text;
+            this.PublicationDate = PublicationDate;
+        }
+    }
+}
