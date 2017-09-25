72418adfbb543952c141366425ddb6bb35635de8
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-25 13:14:02 +0200

    Ex01_PostIt

---
+++ week-04/Day-01/Ex01_Post-It/Ex01_Post-It/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Post_It
+{
+    class Program
+    {
+        struct PostIt
+        {
+            public string BackgroundColor;
+            public string Text;
+            public string TextColor;
+
+            public PostIt(string BackgroundColor, string Text, string TextColor) //this is contructor
+            {
+                this.BackgroundColor = BackgroundColor;
+                this.Text = Text;
+                this.TextColor = TextColor;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PostIt postIt1 = new PostIt("Orange", "Idea 1", "Blue");
+            PostIt postIt2 = new PostIt("Pink", "Awesome", "Black");
+            PostIt postIt3 = new PostIt("Yellow", "Superb", "Green");
+
+            //PostIt postIt1;
+            //PostIt postIt2;
+            //PostIt postIt3;
+
+            //postIt1.BackgroundColor = "Orange";
+            //postIt1.Text = "Idea 1";
+            //postIt1.TextColor = "Blue";
+
+            //postIt2.BackgroundColor = "Pink";
+            //postIt2.Text = "Awesome";
+            //postIt2.TextColor = "Black";
+
+            //postIt3.BackgroundColor = "Yellow";
+            //postIt3.Text = "Superb";
+            //postIt3.TextColor = "Green";
+        }
+    }
+}
