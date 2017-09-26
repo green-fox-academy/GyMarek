f15b4d843614afe7f90aa6969e90d1babd2d66d4
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-26 22:55:58 +0200

    Ex01_PallidaClassDone

---
+++ week-04/Day-02/Ex01_Greenfox/Ex01_Greenfox/PallidaClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Greenfox
+{
+    class PallidaClass
+    {
+        private string className;
+        private List<Student> students= new List<Student>();
+        private List<Mentor> mentors = new List<Mentor>();
+
+        public PallidaClass(string className, List<Student> students, List<Mentor> mentors)
+        {
+            this.className = className;
+            students = new List<Student>();
+            mentors = new List<Mentor>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(new Student());           
+        }
+
+        public void AddMentor(Mentor mentor)
+        {
+            mentors.Add(new Mentor());
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Pallida {0} class has {1} students and {2} mentors.", className, students.Count, mentors.Count);
+        }
+    }
+}
