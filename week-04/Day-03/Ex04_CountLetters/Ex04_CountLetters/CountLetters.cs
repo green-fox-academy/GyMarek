1ab77b233964ed60be7084941b7cbb57cd09f01a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-27 17:00:12 +0200

    Ex04_CountLettersFinished

---
+++ week-04/Day-03/Ex04_CountLetters/Ex04_CountLetters/CountLetters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04_CountLetters
+{
+    public class CountLetters
+    {
+        public Dictionary<char, int> LetterCounter(string input)
+        {
+            Dictionary<char, int> output = new Dictionary<char, int>();
+
+            if (input != null)
+            {
+                char[] fromString = input.Replace(" ","").ToCharArray();
+
+                foreach (var character in fromString)
+                {                   
+                    if (!output.ContainsKey(character))
+                    {
+                        output.Add(character, 1);
+                    }
+                    else
+                    {
+                        output[character]++;
+                    }
+                }            
+            }
+            return output;            
+        }
+    }
+}
---
+++ week-04/Day-03/Ex04_CountLetters/TestCountLetters/TestCount.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ex04_CountLetters;
+
+namespace TestCountLetters
+{
+    [TestFixture]
+    public class TestCount
+    {
+        CountLetters countLetter = new CountLetters();
+
+        [Test]
+        public void TestLetterCounterWithString()
+        {
+            string input = "helloworld";
+
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {  
+                { 'h' , 1 },
+                { 'e' , 1 },
+                { 'l' , 3 },
+                { 'o' , 2 },
+                { 'w' , 1 },
+                { 'r' , 1 },
+                { 'd' , 1 }     
+            };
+            Assert.AreEqual(output, countLetter.LetterCounter(input));
+        }
+
+
+        [Test]
+        public void TestLetterCounterWithStringWithSpace()
+        {
+            string input = "hello world";
+
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+                { 'h' , 1 },
+                { 'e' , 1 },
+                { 'l' , 3 },
+                { 'o' , 2 },
+                { 'w' , 1 },
+                { 'r' , 1 },
+                { 'd' , 1 }
+            };
+            Assert.AreEqual(output, countLetter.LetterCounter(input));
+        }
+
+        [Test]
+        public void TestLetterCounterWithEmptyString()
+        {
+            string input = "";
+
+            Dictionary<char, int> output = new Dictionary<char, int>();
+            
+            Assert.AreEqual(output, countLetter.LetterCounter(input));
+        }
+
+        [Test]
+        public void TestLetterCounterWithNull()
+        {
+            string input = null;
+
+            Dictionary<char, int> output = new Dictionary<char, int>();
+
+            Assert.AreEqual(output, countLetter.LetterCounter(input));
+        }
+    }
+}
