ab22b3f7a4eaa3ae49503cd0ebea770f700c0d59
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-27 17:32:02 +0200

    Ex05_FibonacciFinished

---
+++ week-04/Day-03/Ex05_Fibonacci/Ex05_Fibonacci/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_Fibonacci
+{
+    public class Fibonacci
+    {
+        public static double GetNthFibonacciWithRecursion(int n)
+        {
+            double nthFibonacci = 0;
+            if (n == 2)
+            {
+                nthFibonacci = 1;
+            }
+            if (n <= 2)
+            {
+                return nthFibonacci;
+            }
+            return GetNthFibonacciWithRecursion(n - 1) + GetNthFibonacciWithRecursion(n - 2);
+        }
+    }
+}
---
+++ week-04/Day-03/Ex05_Fibonacci/TestFibonacci/TestFibo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ex05_Fibonacci;
+
+namespace TestFibonacci
+{
+    public class TestFibo
+    {
+        [Test]
+        public void TestFibonacciWithGreaterThanZero()
+        {
+            int input = 4;
+
+            double output = 2;
+
+            Assert.AreEqual(output, Fibonacci.GetNthFibonacciWithRecursion(input));
+        }
+
+        [Test]
+        public void TestFibonacciWithLessEqualThanZero()
+        {
+            int input = -10;
+
+            double output = 0;
+
+            Assert.AreEqual(output, Fibonacci.GetNthFibonacciWithRecursion(input));
+        }        
+    }
+}
