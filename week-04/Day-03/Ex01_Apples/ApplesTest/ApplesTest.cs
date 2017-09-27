691d6ccdb3153e73b7ddda3b322be4191f510c1c
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-27 11:44:17 +0200

    Ex01_Apples

---
+++ week-04/Day-03/Ex01_Apples/ApplesTest/ApplesTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex01_Apples;
+
+namespace ApplesTest
+{
+    [TestFixture]
+    class ApplesTest
+    {
+        Apples apple = new Apples();
+
+        static void Main(string[] args)
+        {
+        }
+
+        [TestCase]
+        public void AppleString()
+        {
+            Assert.AreEqual("Apple", apple.GetApple());
+        }
+    }
+}
---
+++ week-04/Day-03/Ex01_Apples/Ex01_Apples/Apples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Apples
+{
+    public class Apples
+    {
+        public string GetApple()
+        {
+            return "Apple";
+        }
+    }
+}
