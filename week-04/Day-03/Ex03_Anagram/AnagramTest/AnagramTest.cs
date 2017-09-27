247d0b9ebda28aae5269698dfa9c8f6175ca00e9
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-27 15:18:09 +0200

    Ex03_AnagramFinished

---
+++ week-04/Day-03/Ex03_Anagram/AnagramTest/AnagramTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ex03_Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTest
+    {
+        //Anagram anagram = new Anagram();
+
+        [Test]
+        public void TestIsAnagramWithAnagram()
+        {
+            //arrange
+            string one = "hello";
+            string two = "olleh";
+
+            //act
+            bool output = true;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one,two));
+        }
+        
+        [Test]
+        public void TestIsAnagramWithUpperAndLowerCase()
+        {
+            //arrange
+            string one = "Hello";
+            string two = "oLleh";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+
+        [Test]
+        public void TestIsAnagramWithSpace()
+        {
+            //arrange
+            string one = "hello";
+            string two = "oll eh";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+
+        [Test]
+        public void TestIsAnagramWithSpecialCaracter()
+        {
+            //arrange
+            string one = "he#llo.";
+            string two = "o#ll.eh";
+
+            //act
+            bool output = true;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+
+        [Test]
+        public void TestIsAnagramWithOneString()
+        {
+            //arrange
+            string one = "hello";
+            string two = "";
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+
+        [Test]
+        public void TestIsAnagramWithTwoEmptyString()
+        {
+            //arrange
+            string one = "";
+            string two = "";
+
+            //act
+            bool output = true;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+
+        [Test]
+        public void TestIsAnagramWithTwoNUllString()
+        {
+            //arrange
+            string one = null;
+            string two = null;
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+
+        [Test]
+        public void TestIsAnagramWithOneNUllString()
+        {
+            //arrange
+            string one = "hello";
+            string two = null;
+
+            //act
+            bool output = false;
+
+            //assert
+            Assert.AreEqual(output, Anagram.IsAnagram(one, two));
+        }
+    }
+}
---
+++ week-04/Day-03/Ex03_Anagram/Ex03_Anagram/Anagram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_Anagram
+{
+    public class Anagram
+    {
+        public static bool IsAnagram(string first, string second)
+        {
+        if (first != null && second != null)
+        {
+            char[] firstChars = first.ToCharArray();
+            Array.Sort(firstChars);
+            char[] secondChars = second.ToCharArray();
+            Array.Sort(secondChars);
+
+            bool anagram = true;
+
+            if (firstChars.Length != secondChars.Length)
+            {
+                anagram = false;
+            }
+            else
+            {
+                for (int i = 0; i < firstChars.Length; i++)
+                {
+                    if (firstChars[i] != secondChars[i])
+                    {
+                        anagram = false;
+                    }
+                }
+            }
+            return anagram;
+        }
+        return false;       
+        }
+    }
+}
