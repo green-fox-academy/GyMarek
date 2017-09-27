fc07491e119bfc7bb7d5f2215e4aaed5a6880d10
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-27 13:47:16 +0200

    Ex02_SumFinished

---
+++ week-04/Day-03/Ex02_Sum/Ex02_Sum/SumList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_Sum
+{
+    public class SumList
+    {
+        public int SumFromList(List<int> list)
+        {
+            if (list != null)
+            {
+                return list.Sum();
+            }
+            return 0;
+        }
+    }
+}
---
+++ week-04/Day-03/Ex02_Sum/SumListTest/SumListTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ex02_Sum;
+
+namespace SumListTest
+{
+    [TestFixture]
+    public class SumListTest
+    {
+        SumList sumList = new SumList();
+
+        [Test]
+        public void TestSumListWithEmpty()
+        {
+            //arrange
+            List<int> test= new List<int>(new int[] {});
+
+            //act
+            int output = 0;
+
+            //assert
+            Assert.AreEqual(output, sumList.SumFromList(test));
+        }
+
+        [Test]
+        public void TestSumListWithOneElement()
+        {
+            //arrange
+            List<int> test = new List<int>(new int[] {5});
+
+            //act
+            int output = 5;
+
+            //assert
+            Assert.AreEqual(output, sumList.SumFromList(test));
+        }
+
+        [Test]
+        public void TestSumListWithMultiplieElements()
+        {
+            //arrange
+            List<int> test = new List<int>(new int[] { -2, 2, 3 });
+
+            //act
+            int output = 3;
+
+            //assert
+            Assert.AreEqual(output, sumList.SumFromList(test));
+        }
+
+        [Test]
+        public void TestSumListWithNull()
+        {
+            //arrange
+            List<int> test = null;
+
+            //act
+            int output = 0;
+
+            //assert
+            Assert.AreEqual(output, sumList.SumFromList(test));
+        }
+    }
+}
