7d4148958b0b209f8f79c3ddc18b9c9a206345e4
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-01 19:39:33 +0200

    Ex07_Sharpie_Finished

---
+++ week-04/Day-01/Ex07_Sharpie/Ex07_Sharpie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharpie = new Sharpie("red", 1, 100);
+
+            sharpie.Use();
+
+            Console.WriteLine("New Sharpie InkAmount: " + sharpie.GetInkAmount());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week-04/Day-01/Ex07_Sharpie/Ex07_Sharpie/Sharpie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Sharpie
+{
+    class Sharpie
+    {
+        public string Color;
+        public float Width;
+        public float InkAmount = 100;
+
+        public Sharpie(string color, float width, float inkAmount)
+        {
+            this.Color = color;
+            this.Width = width;
+            this.InkAmount = inkAmount;
+        }
+
+        public float Use()
+        {
+            return this.InkAmount--;
+        }
+
+        public float GetInkAmount()
+        {
+            return this.InkAmount;
+        }
+    }
+}
