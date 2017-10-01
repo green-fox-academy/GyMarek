1591cdf4be61aac1b4c666b0100fbc9b1b228b33
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-01 14:42:57 +0200

    Ex04_Dice_Finished

---
+++ week-04/Day-01/Ex04_Dice/Ex04_Dice/Dice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04_Dice
+{
+    public class Dice
+    {
+        //    You have a Dice class which has 6 dice
+        //    You can roll all of them with Roll()
+        //    Check the current rolled numbers with GetCurrent()
+        //    You can reroll with Reroll()
+        //    Your task is to get where all dice is a 6
+
+        static Random RandomValue = new Random();
+        static int[] Dices = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+            return Dices;
+        }
+
+        public int[] GetCurrent()
+        {
+            return Dices;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            Dices[k] = new Random().Next(1, 7);
+        }
+    }
+}
---
+++ week-04/Day-01/Ex04_Dice/Ex04_Dice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04_Dice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dice myDice = new Dice();
+
+            myDice.Roll();
+
+            for (int i = 0; i < myDice.GetCurrent().Length; i++)
+            {
+                while (myDice.GetCurrent(i) != 6)
+                {
+                    myDice.Reroll(i);
+                    Console.WriteLine(myDice.GetCurrent(i));
+                }
+            }
+
+            foreach (var dice in myDice.GetCurrent())
+            {
+                Console.Write("{0} ", dice);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
