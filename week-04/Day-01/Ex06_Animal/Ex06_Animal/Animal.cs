bf6354e11d2bbcc8f67ca7a5f409565f84556120
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-01 19:30:38 +0200

    Ex06_Animal_Done

---
+++ week-04/Day-01/Ex06_Animal/Ex06_Animal/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06_Animal
+{
+    public class Animal
+    {
+        public string NameOfAnimal;                            
+        public int Hunger = 50;                               
+        public int Thirst = 50;
+
+        public Animal(string NameOfAnimal, int Hunger, int Thirst)
+        {
+            this.NameOfAnimal = NameOfAnimal;
+            this.Hunger = Hunger;
+            this.Thirst = Thirst;
+        }
+
+        public void Eat()                                       
+        {
+            Hunger--;
+        }
+
+        public void Drink()                                    
+        {
+            Thirst--;
+        }
+
+        public void Play()                                      
+        {
+            Hunger++;
+            Thirst++;
+        }
+
+        public int GetHunger()                                  
+        {
+            return Hunger;
+        }
+
+        public int GetThirst()                                  
+        {
+            return Thirst;
+        }
+    }
+}
---
+++ week-04/Day-01/Ex06_Animal/Ex06_Animal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06_Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal("cat ", 50, 50);
+            cat.Drink();
+            Console.WriteLine("cat " + cat.GetThirst());
+
+            Animal dog = new Animal("dog ", 50, 50);
+            dog.Eat();
+            Console.WriteLine("dog " + dog.GetHunger());
+
+            Animal monkey = new Animal("monkey", 50, 50);
+            monkey.Play();
+            Console.WriteLine("monkey" + monkey.GetThirst());
+            Console.WriteLine("monkey" + monkey.GetHunger());
+
+            Console.ReadKey();
+        }
+    }
+}
