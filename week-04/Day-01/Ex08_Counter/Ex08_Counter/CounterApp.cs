011f930d8d2b2a66d721471038812b7ccefb655a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-02 22:05:36 +0200

    Ex08_Counter_Finished

---
+++ week-04/Day-01/Ex08_Counter/Ex08_Counter/CounterApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08_Counter
+{
+    public class CounterApp
+    {
+        //Create Counter class
+        //which has an integer property
+        //when creating it should have a default value 0 or we can specify it when creating
+        //we can Add(number) to this counter another whole number
+        //or we can Add() without parameters just increasing the counter's value by one
+        //and we can Get() the current value as string
+        //also we can Reset() the value to the initial value
+
+        private int number = 0;
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+
+        public void Add(int addnumber)
+        {
+            number += addnumber;
+        }
+
+        public void Add()
+        {
+            number += 1;
+        }
+
+        public string GetCounter()
+        {
+            return number.ToString();
+        }
+
+        public void ResetCouter()
+        {
+            number = 0;
+        }
+    }
+}
---
+++ week-04/Day-01/Ex08_Counter/Ex08_Counter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CounterApp counterApp = new CounterApp();
+            
+            counterApp.Add(5); 
+            counterApp.Add();
+            Console.WriteLine(counterApp.GetCounter());
+
+            counterApp.ResetCouter();
+            Console.WriteLine(counterApp.GetCounter());
+
+            Console.ReadKey();
+        }
+    }
+}
