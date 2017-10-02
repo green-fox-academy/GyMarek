f36a9371afcfc5cb94ef990c95fd293a2df469da
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-02 23:10:14 +0200

    Ex11_Sharpie_set_working_notFinishedYet

---
+++ week-04/Day-01/Ex10_Sharpie_set/Ex10_Sharpie_set/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10_Sharpie_set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Reuse your Sharpie class
+        //Create SharpieSet class
+        //it contains a list of Sharpie
+        //Add method CountUsable() -> sharpie is usable if it has ink in it
+        //Add method RemoveTrash() -> removes all unusable sharpies
+
+            var sharpie = new Sharpie("red", 1, 100);
+
+            sharpie.Use();
+
+            Console.WriteLine("New Sharpie InkAmount: " + sharpie.GetInkAmount());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week-04/Day-01/Ex10_Sharpie_set/Ex10_Sharpie_set/Sharpie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10_Sharpie_set
+{
+    class Sharpie
+    {
+        public string Color;
+        public float Width;
+        public float InkAmount = 100;
+
+        public Sharpie(string color, float width, float inkAmount)
+        {
+            this.Color = color;
+            this.Width = width;
+            this.InkAmount = inkAmount;
+        }
+
+        public float Use()
+        {
+            return this.InkAmount--;
+        }
+
+        public float GetInkAmount()
+        {
+            return this.InkAmount;
+        }
+    }
+}
