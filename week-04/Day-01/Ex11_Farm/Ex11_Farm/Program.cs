335c704078144e03817d81802928097609e4edc1
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-03 22:49:41 +0200

    Ex11_Farm_Working

--- week-04/Day-01/Ex11_Farm/Ex11_Farm/Program.cs
+++ week-04/Day-01/Ex11_Farm/Ex11_Farm/Program.cs
@@ -10,6 +10,23 @@ namespace Ex11_Farm
     {
         static void Main(string[] args)
         {
+            Animal animal1 = new Animal("horse", 101, 25);
+            Animal animal2 = new Animal("sheep", 33, 50);
+            Animal animal3 = new Animal("chicken", 25, 42);
+            Animal animal4 = new Animal("cow", 44, 20);
+
+            Farm farm = new Farm();
+            farm.Slot = 5;
+
+            farm.AnimalList.Add(animal1);
+            farm.AnimalList.Add(animal2);
+            farm.AnimalList.Add(animal3);
+            farm.AnimalList.Add(animal4);
+            
+            farm.Breed(farm.AnimalList);
+            farm.Slaughter(farm.AnimalList);
+            farm.Print(farm.AnimalList);
+            Console.ReadLine();
         }
     }
 }
