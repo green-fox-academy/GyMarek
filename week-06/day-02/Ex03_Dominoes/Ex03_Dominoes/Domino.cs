3f8cabd4e336d92047dc3face64659a73ef7a24a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-17 17:23:58 +0200

    Comparable Finished

---
+++ week-06/day-02/Ex03_Dominoes/Ex03_Dominoes/Domino.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_Dominoes
+{
+    public class Domino : IComparable
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int CompareTo (object obj)
+        {
+            Domino otherDomino = obj as Domino;
+
+            if (Values[0]==otherDomino.Values[0])
+            {
+                return this.Values[1].CompareTo(otherDomino.Values[1]);
+            }
+            else
+            {
+                return this.Values[0].CompareTo(otherDomino.Values[0]);
+            }
+        }       
+
+        public override string ToString()
+        {
+            return $"({Values[0]}, {Values[1]})";
+        }
+    }
+}
---
+++ week-06/day-02/Ex03_Dominoes/Ex03_Dominoes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_Dominoes
+{
+    public class DominoesNew
+    {
+        public static void Main(string[] args)
+        {
+            var dominoes = InitializeDominoes();
+            dominoes.Sort();
+
+            foreach (var domino in dominoes)
+            {
+                Console.Write(domino);
+            }
+
+            Console.ReadLine();
+        }
+        
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+            return dominoes;
+        }       
+    }
+}
+
