36a9b66f3444d6468c0a338ee31d9782c48d193a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-18 15:33:28 +0200

    Ex07_Cities

---
+++ week-06/day-03/Ex07_Cities/Ex07_Cities/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Cities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH",
+                "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var startEndCitiesQuery = from city in cities
+                                 where city.StartsWith("A") && city.EndsWith("I")
+                                 select city;
+
+            foreach (var selectCity in startEndCitiesQuery)
+            {
+                Console.WriteLine(selectCity);
+            }
+
+            Console.WriteLine();
+
+            var startEndCitiesLambda = cities.Where(x => x.StartsWith("A") && x.EndsWith("I"));
+
+            foreach (var selectCity2 in startEndCitiesLambda)
+            {
+                Console.WriteLine(selectCity2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
