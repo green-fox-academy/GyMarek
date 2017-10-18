db08365148f1de5efe9fbb38f99c4db955b5edbc
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-18 13:04:52 +0200

    Ex04_SquareAgain Finished

---
+++ week-06/day-03/Ex04_SquaredAgain/Ex04_SquaredAgain/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04_SquaredAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 3, 9, 2, 8, 6, 5 }; 
+
+            IEnumerable<int> NumberSquareQuery =
+                from number in n
+                where number*number > 20
+                select number;
+
+            foreach (var number in NumberSquareQuery)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+
+            IEnumerable<int> numberSquareWithLambda = n.Where(y => y*y > 20);
+
+            foreach (var number in numberSquareWithLambda)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
