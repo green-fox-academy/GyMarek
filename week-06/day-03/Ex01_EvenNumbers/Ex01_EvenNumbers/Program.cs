c81ba3114c6e72122ee0906b6640af45df2b0232
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-18 11:12:11 +0200

    Ex01_EvenNumbers Finished

---
+++ week-06/day-03/Ex01_EvenNumbers/Ex01_EvenNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenNumberQuery =
+                from even in n
+                where even %2 == 0
+                select even;
+
+            IEnumerable<int> evenNumberWithLambda = n.Where(y => y % 2 == 0);
+
+            foreach (var number in evenNumberQuery)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var lambdaNumber in evenNumberWithLambda)
+            {
+                Console.Write(lambdaNumber + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
