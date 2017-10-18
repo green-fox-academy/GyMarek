8ed6db64511d9b39757f5082ab85373a08dcca90
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-18 14:48:25 +0200

    Ex05_Frequency Finished

---
+++ week-06/day-03/Ex05_Frequency/Ex05_Frequency/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_Frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyNumbersLinq = 
+                                from number in n                               
+                                group number by number into uniqNumber
+                                select new
+                                {
+                                    uniqNumber.Key,
+                                    Count = (from number in uniqNumber select number).Count()
+                                };
+
+            foreach (var num in frequencyNumbersLinq)
+            {
+                Console.WriteLine(num);
+            }
+
+            var frequencyNumbersWithLambda = n.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var num2 in frequencyNumbersWithLambda)
+            {
+                Console.WriteLine(num2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
