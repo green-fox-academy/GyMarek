a6b874694106045ac161860292f8959e99abcb4a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-18 15:44:00 +0200

    Ex08_Uppercase Finished

---
+++ week-06/day-03/Ex08_UpperCase/Ex08_UpperCase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08_UpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string upperChar = "Time is an illusion. Lunchtime doubly so.";
+
+            var upperCharLinq = from chars in upperChar
+                                where Char.IsUpper(chars)
+                                select chars;
+
+            foreach (var upper in upperCharLinq)
+            {
+                Console.Write(upper + ",");
+            }
+
+            Console.WriteLine();
+
+            var upperCharLambda = upperChar.Where(x => Char.IsUpper(x));
+
+            foreach (var upper2 in upperCharLambda)
+            {
+                Console.Write(upper2 + ",");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
