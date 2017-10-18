3301647fd2ce87eb3011ef57b2860da905a37bb1
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-18 16:51:02 +0200

    Ex09_CharArrayToString Finished

---
+++ week-06/day-03/Ex09_CharArrayToString/Ex09_CharArrayToString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09_CharArrayToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] test = new char[] { 'd', 'g', 'i', 'o', 'f', 'a', };
+            
+            string stringFromArrayQuery = String.Concat(
+                                           from character in test
+                                           select character);
+
+            Console.WriteLine(stringFromArrayQuery);
+
+            string stringFromArrayLambda = String.Concat(test.Select(character => character));
+
+            Console.WriteLine(stringFromArrayLambda);
+
+            Console.ReadLine();
+        }
+    }
+}
