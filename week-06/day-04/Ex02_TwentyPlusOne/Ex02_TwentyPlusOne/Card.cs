17404a0820e07982298d94c974bb1b3492c1cac2
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-19 16:48:48 +0200

    feat(Card Class): Add constructor and enums

---
+++ week-06/day-04/Ex02_TwentyPlusOne/Ex02_TwentyPlusOne/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_TwentyPlusOne
+{
+    class Card
+    {
+        private Rank rank;
+        private Suit suit;
+
+        public Card(Rank rank, Suit suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+        public Rank Rank { get { return rank; } set { rank = value; } }
+        public Suit Suit { get { return suit; } set { suit = value; } }
+    }
+
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Rank
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 10,
+        Queen = 10,
+        King = 10,
+        Ace = 11
+    }
+}
---
+++ week-06/day-04/Ex02_TwentyPlusOne/Ex02_TwentyPlusOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_TwentyPlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
