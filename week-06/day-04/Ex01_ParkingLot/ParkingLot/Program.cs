5d70ee3e31a8ea8e2477de3f89752e6f369404d4
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-19 14:39:38 +0200

    feat(ParkingLot): Car count by type and color - Query and Method

--- week-06/day-04/Ex01_ParkingLot/ParkingLot/Program.cs
+++ week-06/day-04/Ex01_ParkingLot/ParkingLot/Program.cs
@@ -14,11 +14,13 @@ namespace ParkingLot
             Random random = new Random();
             List<Car> cars = new List<Car>();
 
+            //Generate Random Car List
             for (int i = 0; i < carNumber; i++)
             {
                 cars.Add(new Car((Car.CarColor)random.Next(1, 10), (Car.CarType)random.Next(1, 10)));
             }
 
+            //Query Type
             var sameType = from carType in cars
                            group carType by carType.type into selectType
                            select new
@@ -32,6 +34,19 @@ namespace ParkingLot
                 Console.WriteLine(itemType);
             }
 
+            Console.WriteLine();
+
+            //Method Type
+            var sameTypeLambda = cars.GroupBy(car => car.type).ToDictionary(car => car.Key, car => car.Count());
+
+            foreach (var itemTypeLambda in sameTypeLambda)
+            {
+                Console.WriteLine(itemTypeLambda);
+            }
+
+            Console.WriteLine();
+
+            //Query Color
             var sameColor = from carColor in cars
                            group carColor by carColor.color into selectColor
                            select new
@@ -43,6 +58,16 @@ namespace ParkingLot
             foreach (var itemColor in sameColor)
             {
                 Console.WriteLine(itemColor);
+            }
+
+            Console.WriteLine();
+
+            //Method Color
+            var sameColorLambda = cars.GroupBy(car => car.color).ToDictionary(car => car.Key, car => car.Count());
+
+            foreach (var itemColorLambda in sameColorLambda)
+            {
+                Console.WriteLine(itemColorLambda);
             }
 
             Console.ReadKey();
