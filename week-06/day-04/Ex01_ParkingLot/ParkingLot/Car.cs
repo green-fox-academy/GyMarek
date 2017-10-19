7a2c3e426712ec351320517ccb623b406932623c
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-19 11:41:34 +0200

    feat(ParkingLot): Car class and Random Car list added

---
+++ week-06/day-04/Ex01_ParkingLot/ParkingLot/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public class Car
+    {
+        public Car(CarColor color,CarType type)
+        {
+            this.color = color;
+            this.type = type;
+        }
+
+        public CarColor color { get; set; }
+        public CarType type { get; set; }
+
+        public enum CarType
+        {
+            Honda,
+            Suzuki,
+            Ford,
+            Renault,
+            Skoda,
+            Bmw,
+            Mercedes,
+            Audi,
+            Dacia,
+            Toyota
+        }
+
+        public enum CarColor
+        {
+            Blue,
+            Red,
+            White,
+            Black,
+            Yellow,
+            Grey,
+            Green,
+            Pink,
+            Orange,
+            Purple
+        }
+
+        public override string ToString()
+        {
+            return $"{color}, {type}";
+        }
+    }
+}
---
+++ week-06/day-04/Ex01_ParkingLot/ParkingLot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carNumber = 256;
+            Random random = new Random();
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < carNumber; i++)
+            {
+                cars.Add(new Car(((Car.CarColor)random.Next(typeof(Car.CarColor).GetEnumNames().Length)),
+                ((Car.CarType)random.Next(typeof(Car.CarType).GetEnumNames().Length))));
+            }
+                        
+            Console.ReadKey();
+        }
+    }
+}
