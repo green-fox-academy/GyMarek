a471179d2e88dc9c6bfd43dc837d9d3d6a49da07
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-24 13:41:52 +0200

    Feat(FirstASP): Add Hello world method

---
+++ week-07/day-01/FirstASP/FirstASP/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("Hello");
+        }
+
+        [Route("hello")]
+        public IActionResult Hello()
+        {
+            return Content("Hello World!");
+        }
+    }
+}
