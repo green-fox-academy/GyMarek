2ec50c4c0a7fad9c547f9addcb44bd7b3aa3e63a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-24 13:47:49 +0200

    Feat(FirstASP): Add Greeting Class and RESTController

---
+++ week-07/day-01/FirstASP/FirstASP/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstASP.Models
+{
+    public class Greeting
+    {
+        long Id;
+        string Content;
+    }
+}
