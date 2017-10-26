e5946e40ae407b45e9e96e3e9c6c5dee2293e19b
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-26 17:08:37 +0200

    Feat(GreetSome): finished

---
+++ week-07/day-03/Ex02_GreetSomeone/Ex02_GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ex02_GreetSomeone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ex02_GreetSomeone.Controllers
+{
+    public class HomeController : Controller
+    {
+        Greet greet;
+
+        public HomeController(Greet greet)
+        {
+            this.greet = greet;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult IndexWithForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("submit")]
+        public IActionResult Index(string name)
+        {
+            greet.Name = name;
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("submit")]
+        public IActionResult Index()
+        {           
+            return View(greet);
+        }
+    }
+}
---
+++ week-07/day-03/Ex02_GreetSomeone/Ex02_GreetSomeone/Models/Greet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex02_GreetSomeone.Models
+{
+    public class Greet
+    {
+        public string Name { get; set; } 
+    }
+}
