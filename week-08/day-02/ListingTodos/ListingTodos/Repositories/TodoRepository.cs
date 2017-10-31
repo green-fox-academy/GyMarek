672de51d423d020b3ec93ec271d2debdc6ab644a
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-31 14:09:23 +0100

    Feat(ListingTodos): create list view

--- week-08/day-02/ListingTodos/ListingTodos/Repositories/TodoRepository.cs
+++ week-08/day-02/ListingTodos/ListingTodos/Repositories/TodoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListingTodos.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace ListingTodos.Repositories
 {
     public class TodoRepository
     {
+        TodoContext TodoContext;
+
+        public TodoRepository(TodoContext todoContext)
+        {
+            TodoContext = todoContext;
+        }
     }
 }
