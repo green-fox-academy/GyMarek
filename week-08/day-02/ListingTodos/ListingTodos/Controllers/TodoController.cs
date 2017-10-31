71757d47223a80ee7859a29427a43159f8507c99
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-31 13:59:45 +0100

    Feat(ListingTodos): Add DbContext, Repository class and inject to controller

--- week-08/day-02/ListingTodos/ListingTodos/Controllers/TodoController.cs
+++ week-08/day-02/ListingTodos/ListingTodos/Controllers/TodoController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using ListingTodos.Repositories;
 
 namespace ListingTodos.Controllers
 {
     [Route("")]
     public class TodoController : Controller
     {
+        TodoRepository TodoRepository;
+
         [Route("/list")]
         public IActionResult List()
         {
