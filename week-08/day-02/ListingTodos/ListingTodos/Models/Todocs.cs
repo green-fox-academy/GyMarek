185f850ba5f100221aceb5bf2704cc2a87af6392
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-31 13:07:33 +0100

    Feat(ListingTodos): Add dependencies, add folder structure, add Todo class

---
+++ week-08/day-02/ListingTodos/ListingTodos/Models/Todocs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingTodos.Models
+{
+    public class Todocs
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent = false;
+        public bool IsDone = false;
+    }
+}
