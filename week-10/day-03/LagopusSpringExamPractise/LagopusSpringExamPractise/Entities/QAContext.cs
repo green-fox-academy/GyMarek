cc8b1ec225afb55f07c9d4d04dbc11b3b5817a03
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-11-15 11:50:06 +0100

    Feat(ExamPractise): add nuget packages

---
+++ week-10/day-03/LagopusSpringExamPractise/LagopusSpringExamPractise/Entities/QAContext.cs
@@ -0,0 +1,19 @@
+﻿using LagopusSpringExamPractise.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagopusSpringExamPractise.Entities
+{
+    public class QAContext : DbContext
+    {
+        public QAContext(DbContextOptions<QAContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<QandA> QA { get; set; } 
+    }
+}
---
+++ week-10/day-03/LagopusSpringExamPractise/LagopusSpringExamPractise/Models/QandA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagopusSpringExamPractise.Models
+{
+    public class QandA
+    {
+        public int QandAId { get; set; }
+        public string Quetion { get; set; }
+        public string Answer { get; set; }
+    }
+}
